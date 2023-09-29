5e011d9d239f99b62f115d7e937b0755c6217c97
AlanMestezk <alanmestezk98@gmail.com>
2023-09-28 21:46:19 -0300

    deploy teste

--- src/routes.tsx
+++ src/routes.tsx
@@ -20,7 +20,7 @@ export const router = createBrowserRouter(
             element: <Layout/>,
             children:[
                 {
-                    path: '/websimpson',
+                    path: '/',
                     element: <Home/>
                 },
                 {
